33ea6d4aa50da776d580b84104b2f5b162f51d95
Konstantin Khaletsky <konstkhal@gmail.com>
2022-07-24 22:23:38 +0300

    feat(api-catch-errors): add catch at a very end of each api call
    Need to catch possible errors at the very end of any server request.

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,8 @@
 
 import React from 'react';
 //import profile__photo from '../images/image.png';
-import profile__change from '../images/pen.svg';
-import link__add from '../images/Vector-1.svg';
+import profileChangeImage from '../images/pen.svg';
+import linkAddImage from '../images/Vector-1.svg';
 import { api } from '../utils/api.js';
 import Card from './Card.js';
 
@@ -21,19 +21,21 @@ function Main({
   const [cards, setCards] = React.useState([]);
 
   React.useEffect(() => {
-    api.getUserInfo().then((userData) => {
-      setUserName(userData.name);
-      setUserDescription(userData.about);
-      setUserAvatar(userData.avatar);
-    });
-    api.getInitialCards().then((cardData) => {
-      setCards(cardData);
-    });
+    api
+      .getUserInfo()
+      .then((userData) => {
+        setUserName(userData.name);
+        setUserDescription(userData.about);
+        setUserAvatar(userData.avatar);
+      })
+      .catch((err) => console.log(err));
+    api
+      .getInitialCards()
+      .then((cardData) => {
+        setCards(cardData);
+      })
+      .catch((err) => console.log(err));
   }, []);
-
-  const profile__photo = userAvatar;
-  const profile__name = userName;
-  const profile__role = userDescription;
 
   return (
     <main className='content'>
@@ -41,7 +43,7 @@ function Main({
         <div className='profile__photo-container'>
           <div className='profile__cover-overlay'></div>
           <img
-            src={profile__photo}
+            src={userAvatar}
             className='profile__photo'
             alt='Alt placeholder'
           />
@@ -50,7 +52,7 @@ function Main({
             className='button profile__link-change profile__link-change_place_image'
             onClick={() => onEditAvatarClick()}>
             <img
-              src={profile__change}
+              src={profileChangeImage}
               className='profile__image-change profile__image-change_place_image'
               alt='Alt placeholder'
             />
@@ -58,20 +60,20 @@ function Main({
         </div>
 
         <div className='profile__info'>
-          <h1 className='profile__name'>{profile__name}</h1>
+          <h1 className='profile__name'>{userName}</h1>
 
           <button
             type='button'
             className='button profile__link-change profile__link-change_place_profile'
             onClick={onEditProfileClick}>
             <img
-              src={profile__change}
+              src={profileChangeImage}
               className='profile__image-change'
               alt='Alt placeholder'
             />
           </button>
 
-          <p className='profile__role'>{profile__role}</p>
+          <p className='profile__role'>{userDescription}</p>
         </div>
 
         <button
@@ -79,7 +81,7 @@ function Main({
           className='button profile__link-add'
           onClick={onAddPlaceClick}>
           <img
-            src={link__add}
+            src={linkAddImage}
             className='profile__image-add'
             alt='Alt placeholder'
           />
