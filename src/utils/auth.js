218bef00cda9861fe0c274f61e3905dcc1015c31
Konstantin Khaletsky <konstkhal@gmail.com>
2022-10-01 16:33:05 +0300

    Errorneous commit

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -9,13 +9,10 @@ class Auth extends Authorization {
     method: 'POST',
     body: JSON.stringify({ password: user.password, email: user.email }),
   })
-<<<<<<< HEAD
-=======
   .then((res) => res.json())
   .then((data) => {
     if (data.message) throw new Error(data.message);
   });
->>>>>>> feat__auth_response-processing
 }
 
   authenticate = (user) => {
@@ -24,12 +21,6 @@ class Auth extends Authorization {
     method: 'POST',
     body: JSON.stringify({ password: user.password, email: user.email }),
   })
-<<<<<<< HEAD
-};
-
-  validateToken = (token) => {
-    this._headers = {...'Content-Type', 'Authorization': `Bearer ${token}`}
-=======
   .then((res) => res.json())
   .then((data) => {
     if (!data.token) throw new Error(data.message);
@@ -39,19 +30,15 @@ class Auth extends Authorization {
 
   validateToken = (token) => {
     this._headers = {...this._headers, 'Authorization': `Bearer ${token}`}
->>>>>>> feat__auth_response-processing
   return this._customFetch(`${this._baseUrl}/users/me`, {
     headers: this._headers,
     method: 'GET',
   })
-<<<<<<< HEAD
-=======
   .then((res) => res.json())
   .then((data) => {
       if (!data) throw new Error(data.message);
       return data;
     });
->>>>>>> feat__auth_response-processing
 };
 }
 
