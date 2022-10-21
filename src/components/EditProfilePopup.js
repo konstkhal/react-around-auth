bf7bf940cf3fafce9d06bc2e778d319e73452ee6
Konstantin Khaletsky <konstkhal@gmail.com>
2022-10-21 16:13:37 +0300

    fix(popup): :bug: Fix email keep in header on close

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -13,78 +13,79 @@ import React from 'react';
 import { UserContext } from '../contexts/CurrentUserContext';
 
 export default function EditProfilePopup({
-  isOpen,
-  onClose,
-  onUpdateUser,
+	isOpen,
+	onClose,
+	onUpdateUser,
 }) {
-  const [name, setName] = React.useState('');
-  const [description, setDescription] = React.useState('');
-  const currentUser = React.useContext(UserContext);
+	const [name, setName] = React.useState('');
+	const [description, setDescription] = React.useState('');
+	const currentUser = React.useContext(UserContext);
 
-  /**
-   * Inside the EditProfilePopup add the name and description state variables
-   * and bind them to the input fields, making them manageable.
-   * Don't forget about the onChange() handlers.
-   *
-   * After loading the current user from the API
-   * their data will be used in managed components.
-   */
+	/**
+	 * Inside the EditProfilePopup add the name and description state variables
+	 * and bind them to the input fields, making them manageable.
+	 * Don't forget about the onChange() handlers.
+	 *
+	 * After loading the current user from the API
+	 * their data will be used in managed components.
+	 */
 
-  React.useEffect(() => {
-    setName(currentUser.name || '');
-    setDescription(currentUser.about || '');
-  }, [currentUser]);
+	React.useEffect(() => {
+		setName(currentUser.name || '');
+		setDescription(currentUser.about || '');
+	}, [currentUser, isOpen]);
 
-  const handleNameChange = (event) =>
-    setName(event.target.value);
+	const handleNameChange = (event) =>
+		setName(event.target.value);
 
-  const handleRoleChange = (event) =>
-    setDescription(event.target.value);
+	const handleRoleChange = (event) =>
+		setDescription(event.target.value);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    onUpdateUser({ name, about: description });
-  };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		onUpdateUser({ name, about: description });
+	};
 
-  return (
-    <PopupWithForm
-      isOpen={isOpen}
-      onClose={onClose}
-      title='Edit profile'
-      name='editProfile'
-      onSubmit={handleSubmit}>
-      <input
-        id='name-input'
-        onChange={handleNameChange}
-        value={name}
-        placeholder='Input name'
-        type='text'
-        className='form__input form__input_type_name'
-        name='profilenameInput'
-        minLength='2'
-        maxLength='40'
-        required
-      />
-      <span
-        id='name-input-error'
-        className='form__input-error'
-      />
-      <input
-        id='role-input'
-        onChange={handleRoleChange}
-        value={description}
-        type='text'
-        className='form__input form__input_type_role'
-        name='profileFormRoleInput'
-        minLength='2'
-        maxLength='200'
-        placeholder='Input role'
-        required
-      />
-      <span
-        id='role-input-error'
-        className='form__input-error'
-      />
-    </PopupWithForm>
-  );
+	return (
+		<PopupWithForm
+			isOpen={isOpen}
+			onClose={onClose}
+			title='Edit profile'
+			name='editProfile'
+			onSubmit={handleSubmit}
+		>
+			<input
+				id='name-input'
+				onChange={handleNameChange}
+				value={name}
+				placeholder='Input name'
+				type='text'
+				className='form__input form__input_type_name'
+				name='profilenameInput'
+				minLength='2'
+				maxLength='40'
+				required
+			/>
+			<span
+				id='name-input-error'
+				className='form__input-error'
+			/>
+			<input
+				id='role-input'
+				onChange={handleRoleChange}
+				value={description}
+				type='text'
+				className='form__input form__input_type_role'
+				name='profileFormRoleInput'
+				minLength='2'
+				maxLength='200'
+				placeholder='Input role'
+				required
+			/>
+			<span
+				id='role-input-error'
+				className='form__input-error'
+			/>
+		</PopupWithForm>
+	);
 }
