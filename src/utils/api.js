c15605fa0aaf66378161606d07411a18140448e5
Konstantin Khaletsky <konstkhal@gmail.com>
2022-10-22 21:16:46 +0300

    style(Authorization&Registration): :wastebasket: Style api class export instructions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,73 +1,73 @@
 /** @format */
 
-export class Api {
-  constructor({ baseUrl, headers }) {
-    this._baseUrl = baseUrl;
-    this._headers = headers;
-  }
-  _customFetch = async (url, headers) => {
-    const response = await fetch(url, headers);
-    if (response.ok) {
-      return response.json();
-    } else {
-      return Promise.reject(response.statusText);
-    }
-  };
+class Api {
+	constructor({ baseUrl, headers }) {
+		this._baseUrl = baseUrl;
+		this._headers = headers;
+	}
+	_customFetch = async (url, headers) => {
+		const response = await fetch(url, headers);
+		if (response.ok) {
+			return response.json();
+		} else {
+			return Promise.reject(response.statusText);
+		}
+	};
 
-  init = () =>
-    Promise.all([
-      this._getInitialCards(),
-      this._getUserInfo(),
-    ]);
+	init = () =>
+		Promise.all([
+			this._getInitialCards(),
+			this._getUserInfo(),
+		]);
 
-  _getInitialCards() {
-    return this._customFetch(`${this._baseUrl}/cards`, {
-      headers: this._headers,
-    });
-  }
-  _getUserInfo() {
-    return this._customFetch(`${this._baseUrl}/users/me`, {
-      headers: this._headers,
-    });
-  }
+	_getInitialCards() {
+		return this._customFetch(`${this._baseUrl}/cards`, {
+			headers: this._headers,
+		});
+	}
+	_getUserInfo() {
+		return this._customFetch(`${this._baseUrl}/users/me`, {
+			headers: this._headers,
+		});
+	}
 
-  setAvatarLink(data) {
-    return this._customFetch(
-      `${this._baseUrl}/users/me/avatar`,
-      {
-        headers: this._headers,
-        method: 'PATCH',
-        body: JSON.stringify({ avatar: data }),
-      }
-    );
-  }
+	setAvatarLink(data) {
+		return this._customFetch(
+			`${this._baseUrl}/users/me/avatar`,
+			{
+				headers: this._headers,
+				method: 'PATCH',
+				body: JSON.stringify({ avatar: data }),
+			}
+		);
+	}
 
-  setUserInfo(data) {
-    return this._customFetch(`${this._baseUrl}/users/me`, {
-      headers: this._headers,
-      method: 'PATCH',
-      body: JSON.stringify(data),
-    });
-  }
+	setUserInfo(data) {
+		return this._customFetch(`${this._baseUrl}/users/me`, {
+			headers: this._headers,
+			method: 'PATCH',
+			body: JSON.stringify(data),
+		});
+	}
 
-  createCard(data) {
-    return this._customFetch(`${this._baseUrl}/cards`, {
-      headers: this._headers,
-      method: 'POST',
-      body: JSON.stringify(data),
-    });
-  }
+	createCard(data) {
+		return this._customFetch(`${this._baseUrl}/cards`, {
+			headers: this._headers,
+			method: 'POST',
+			body: JSON.stringify(data),
+		});
+	}
 
-  deleteCard(cardId) {
-    return this._customFetch(
-      `${this._baseUrl}/cards/${cardId}`,
-      {
-        headers: this._headers,
-        method: 'DELETE',
-      }
-    );
-  }
-  /*   likeCard(cardId) {
+	deleteCard(cardId) {
+		return this._customFetch(
+			`${this._baseUrl}/cards/${cardId}`,
+			{
+				headers: this._headers,
+				method: 'DELETE',
+			}
+		);
+	}
+	/*   likeCard(cardId) {
     return this._customFetch(
       `${this._baseUrl}/cards/likes/${cardId}`,
       {
@@ -87,26 +87,25 @@ export class Api {
     );
   } */
 
-  handleLikeCardStatus(cardId, isLiked) {
-    return this._customFetch(
-      `${this._baseUrl}/cards/likes/${cardId}`,
-      {
-        headers: this._headers,
-        method: isLiked ? 'DELETE' : 'PUT',
-      }
-    );
-  }
+	handleLikeCardStatus(cardId, isLiked) {
+		return this._customFetch(
+			`${this._baseUrl}/cards/likes/${cardId}`,
+			{
+				headers: this._headers,
+				method: isLiked ? 'DELETE' : 'PUT',
+			}
+		);
+	}
 
-  // other methods for working with the API
+	// other methods for working with the API
 }
 
 const api = new Api({
-  baseUrl: 'https://around.nomoreparties.co/v1/group-12',
-  headers: {
-    authorization: 'cfbd7707-a110-44ae-8aa8-630296f53c66',
-    'Content-Type': 'application/json',
-  },
+	baseUrl: 'https://around.nomoreparties.co/v1/group-12',
+	headers: {
+		authorization: 'cfbd7707-a110-44ae-8aa8-630296f53c66',
+		'Content-Type': 'application/json',
+	},
 });
 
-
-export { api, Api as Authorization};
+export { api, Api };
